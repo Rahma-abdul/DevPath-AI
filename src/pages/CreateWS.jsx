4b6ae0e62c6094d4c2bf53e84d4d9daa5f662200
Rahma-abdul <rahma2002lola@gmail.com>
2026-06-27 00:14:35 +0300

    Add features and modify implemented

--- src/pages/CreateWS.jsx
+++ src/pages/CreateWS.jsx
@@ -1,13 +1,45 @@
 import { useState } from "react";
 import "../styles/newWS.css";
-import Landing from "./Landing";
+
 
 
 function CreateWS(){
 
     const [stage ,setStage] = useState(1);
     const [loading , setLoading] = useState(false);
+    const [newFeature , setNewFeature] = useState("");
+    const [showModal , setShowModal] =useState(false);
 
+    const [showModify , setShowModify] =useState(false);
+    const [complexity , setComplexity] = useState("Medium");
+    const [duration , setDuration] = useState("73");
+
+    const [showStack , setShowStack] =useState(false);
+
+
+
+
+    const [features , setFeatures] = useState([
+        "Authentication",
+        "Dashboard",
+        "Notifications",
+        "Analytics",
+        "Chatbot"
+    ]);
+
+    const addFeature = () =>{
+        if (newFeature.trim() === "") return;
+        const featureNorm = newFeature.trim().toLowerCase().replace(/\b\w/g, (char) => char.toUpperCase()); 
+        // console.log("Feature is:" , featureNorm);
+        setFeatures([...features, featureNorm]);
+        setNewFeature("");
+        setShowModal(false);
+    };
+
+    const modifyComplexity_Duration =() =>{
+        // Close modal after user saves changes
+        setShowModify(false);
+    }
     // {stage === 1 && <IdeaCard />}  // mame and idea 
     // {stage === 2 && <FeatureCard />} // extracted features
     // {stage === 3 && <ScopeCard />} // goal, timeline, budget and focus
@@ -37,16 +69,14 @@ function CreateWS(){
                 <div className={`progress-step ${stage>=4? "active": ""}`}>Feasibility</div>
                 <div className={`progress-step ${stage>=5? "active": ""}`}>Architecture</div>
             </div>
-            {/* <div className="progress-meter">
-                     <span style={{width: "2%"}}></span>
-                </div> */}
+            
             {(loading && stage === 1 )? (
                 <>
                 <div className="progress-meter">
                      <span style={{width: "8%"}}></span>
                     </div>
                 <div className="ws-card">
-                    <h3>AI Is Analyzing your project</h3>
+                    <h3>Analyzing <span style={{color: "white"}}>your project</span></h3>
                     <div className="loading-list">
                         <p>Understanding project idea</p>
                         <p>Extracting features</p>
@@ -59,7 +89,7 @@ function CreateWS(){
                      <span style={{width: "50%"}}></span>
                     </div>
                 <div className="ws-card">
-                    <h3>AI Is Running Feasibility Analysis</h3>
+                    <h3>Running <span style={{color: "white"}}>Feasibility Analysis</span></h3>
                     <div className="loading-list">
                         <p>Calculation Complexity</p>
                         <p>Estimating Duration</p>
@@ -73,7 +103,7 @@ function CreateWS(){
                      <span style={{width: "70%"}}></span>
                     </div>
                 <div className="ws-card">
-                    <h3>AI Is Generating Architecture Recommendation</h3>
+                    <h3><span style={{color: "white"}}>Generating</span> Architecture Recommendation</h3>
                     <div className="loading-list">
                         <p>Generating list of stack options</p>
                         <p>Collecting pros and cons</p>
@@ -101,7 +131,7 @@ function CreateWS(){
                     <h2>Workspace Name</h2>
                     <input type="text" placeholder="Enter your workspace name" />
                     <h2>Project Idea</h2>
-                    <textarea placeholder="Describe your project idea or the problem you're trying to solve and how you plan on solving it" /> 
+                    <textarea placeholder="Describe your project idea or the problem you're trying to solve and how you plan on solving it"  /> 
                     <button onClick={nextStage}>Continue</button>
                     </div>
                     </>
@@ -115,24 +145,16 @@ function CreateWS(){
                     <h3><span style={{color: "white"}}>AI Extracted</span> Features</h3>
                     <h2>Pick which ones fit your idea best</h2>
                     <div className="features-container">
-                    <h2>
-                        <input type="checkbox" defaultChecked />
-                        Authentication
-                    </h2>
-                    <h2>
-                        <input type="checkbox" defaultChecked />
-                        Profiles
-                    </h2>
-                    <h2>
-                        <input type="checkbox" defaultChecked />
-                        Dashboard
-                    </h2>
-                    <h2>
-                        <input type="checkbox" defaultChecked />
-                        Chatbot
-                    </h2>
+                   
+                    {features.map(feature => (
+                        <h2 key={feature}>
+                            <input type="checkbox" defaultChecked />
+                            {feature}
+                        </h2>
+                    ))}
+
                     </div>
-                    <button>Add Feature</button>
+                    <button onClick={() => setShowModal(true)}>+ Add Feature</button>
                     <button onClick={nextStage_noLoad}>Confirm</button>
                     </div>
                     </>
@@ -193,8 +215,8 @@ function CreateWS(){
                     <h3><span style={{color: "white"}}>AI ran</span> Feasibility Analysis</h3>
             
                     <div className="features-container">
-                        <h2><span style={{color: "#958fe5" , fontWeight: "800"}}>Complexity:</span> <span style={{fontFamily: "`Inter`, sans-serif" , letterSpacing: "-0.05em" , fontWeight: "400"}}>Medium</span></h2>
-                        <h2><span style={{color: "#958fe5" , fontWeight: "800"}}>Implementation Estimated Duration:</span> <span style={{fontFamily: "`Inter`, sans-serif" , letterSpacing: "-0.05em" , fontWeight: "400"}}>47 hours</span></h2>
+                        <h2><span style={{color: "#958fe5" , fontWeight: "800"}}>Complexity:</span> <span style={{fontFamily: "`Inter`, sans-serif" , letterSpacing: "-0.05em" , fontWeight: "400"}}>{complexity}</span></h2>
+                        <h2><span style={{color: "#958fe5" , fontWeight: "800"}}>Implementation Estimated Duration:</span> <span style={{fontFamily: "`Inter`, sans-serif" , letterSpacing: "-0.05em" , fontWeight: "400"}}>{duration} hours</span></h2>
                         <h2 style={{color: "#958fe5" , fontWeight: "800"}}>Possible Risks:</h2>
                         <div className="regular-list">
                             <p>Time might be too tight</p>
@@ -202,7 +224,7 @@ function CreateWS(){
                             <p>Might need to change the budget plan</p>
                         </div>
                     </div>
-                    <button>Modify</button>
+                    <button onClick={()=>setShowModify(true)}>Modify</button>
                     <button onClick={nextStage}>Confirm</button>
                     </div>
                     </>
@@ -223,10 +245,10 @@ function CreateWS(){
                         </div>
                     <h2>Why?</h2>
                     <div className="regular-list">
-                            <p><span style={{color: "#958fe5" , fontSize: "1.4rem"}}>React simplifies building dynamic user interfaces through a modular, predictable, and highly efficient development paradigm.</span></p>
-                            <p><span style={{color: "#958fe5" , fontSize: "1.4rem"}}>Express provides a thin layer of fundamental web application features without obscuring Node.js features. </span></p>
-                            <p><span style={{color: "#958fe5" , fontSize: "1.4rem"}}>Supabase pairs a real PostgreSQL database with an automated, built-in suite of backend features.</span></p>
-                            <p><span style={{color: "#958fe5" , fontSize: "1.4rem"}}>Vercel offers zero-configuration Git integration, instant preview URLs for every pull request, and automatic global edge rendering.</span></p>
+                            <p><span style={{color: "#958fe5" , fontSize: "1.1rem"}}>React simplifies building dynamic user interfaces through a modular, predictable, and highly efficient development paradigm.</span></p>
+                            <p><span style={{color: "#958fe5" , fontSize: "1.1rem"}}>Express provides a thin layer of fundamental web application features without obscuring Node.js features. </span></p>
+                            <p><span style={{color: "#958fe5" , fontSize: "1.1rem"}}>Supabase pairs a real PostgreSQL database with an automated, built-in suite of backend features.</span></p>
+                            <p><span style={{color: "#958fe5" , fontSize: "1.1rem"}}>Vercel offers zero-configuration Git integration, instant preview URLs for every pull request, and automatic global edge rendering.</span></p>
                         </div>
 
                     <button>Other Stack Options</button>
@@ -239,8 +261,120 @@ function CreateWS(){
             )
             }
 
-            
-           
+            {showModal && (
+            <div className="modal-overlay">
+
+                <div className="modal-card">
+
+                    <h2>Add Feature</h2>
+
+                    <p>
+                        Add a feature that wasn't suggested by Astra AI.
+                    </p>
+
+                    <input
+                        type="text"
+                        placeholder="Example: Dark Mode"
+                        value={newFeature}
+                        onChange={(e)=>setNewFeature(e.target.value)}
+                    />
+
+                    <div className="modal-buttons">
+
+                        <button
+                            type="button"
+                            className="cancel-btn"
+                            onClick={()=>setShowModal(false)}
+                        >
+                            Cancel
+                        </button>
+
+                        <button
+                            type="button"
+                            onClick={addFeature}
+                        >
+                            Add
+                        </button>
+
+                    </div>
+
+                </div>
+
+            </div>
+            )}
+
+            {showModify && (
+            <div className="modal-overlay">
+
+                <div className="modal-card">
+
+                        <h2>Complexity</h2>
+                        <div className="timeline-container">
+                        <h2 style={{fontSize: "1.1rem"}}>
+                            <input
+                                type="radio"
+                                name="complexity"
+                                value="Beginner"
+                                checked={complexity === "Beginner"}
+                                onChange={(e) => setComplexity(e.target.value)}
+                            />
+                            Beginner
+                        </h2>
+                        <h2 style={{fontSize: "1.1rem"}}>
+                            <input
+                                type="radio"
+                                name="complexity"
+                                value="Medium"
+                                checked={complexity === "Medium"}
+                                onChange={(e) => setComplexity(e.target.value)}
+                            />
+                            Medium
+                        </h2>
+                        <h2 style={{fontSize: "1.1rem"}}>
+                            <input
+                                type="radio"
+                                name="complexity"
+                                value="Expert"
+                                checked={complexity === "Expert"}
+                                onChange={(e) => setComplexity(e.target.value)}
+                            />
+                            Expert
+                        </h2>
+                        </div>
+                    
+                    <h2>Duration</h2>
+                    <p>Change the hours you want to complete the whole project in</p>
+
+                    <input
+                        type="number"
+                        placeholder="Example: 53"
+                        value={duration}
+                        onChange={(e)=>setDuration(e.target.value)}
+                    />
+
+                    <div className="modal-buttons">
+
+                        <button
+                            type="button"
+                            className="cancel-btn"
+                            onClick={()=>setShowModify(false)}
+                        >
+                            Cancel
+                        </button>
+
+                        <button
+                            type="button"
+                            onClick={modifyComplexity_Duration}
+                        >
+                            Save
+                        </button>
+
+                    </div>
+
+                </div>
+
+            </div>
+            )}
 
         </div>
     );
